319ae8fa58ec83f063fefa249e4cd86421bf1fd8
rushi-smb <rushi@smbios.co.in>
2023-09-12 22:50:52 +0530

    Load Featured Products, Get only featured products on home screen

--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -49,5 +49,12 @@ namespace BlazorEcommerce.Server.Controllers
             var result = await _productService.GetProductSearchSuggestions(searchText);
             return Ok(result);
         }
+        
+        [HttpGet("featured")]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetFeaturedProduct()
+        {
+            var result = await _productService.GetFeatureProducts();
+            return Ok(result);
+        }
     }
 }
