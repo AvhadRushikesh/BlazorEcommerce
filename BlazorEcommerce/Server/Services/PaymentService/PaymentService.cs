7db85427b7d10055db8ed818285b2b857cfca625
rushi-smb <rushi@smbios.co.in>
2023-09-22 13:54:36 +0530

    Create a Checkout Session onn the Server

---
+++ BlazorEcommerce/Server/Services/PaymentService/PaymentService.cs
@@ -0,0 +1,57 @@
+﻿using Stripe;
+using Stripe.Checkout;
+
+namespace BlazorEcommerce.Server.Services.PaymentService
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly ICartService _cartService;
+        private readonly IAuthService _authService;
+        private readonly IOrderService _orderService;
+
+        public PaymentService(ICartService cartService, IAuthService authService, IOrderService orderService)
+        {
+            StripeConfiguration.ApiKey = "sk_test_51Nt1YXSCFuXMpYe6YkTGdhcjI2mi08G95lX86q9JCewjhvNqDMyXm7Cq6sAZQBioYWmaSAG3YAuITs6epTHeUS7B00vTx8plvX";
+            _cartService = cartService;
+            _authService = authService;
+            _orderService = orderService;
+        }
+
+        public async Task<Session> CreateCheckoutSession()
+        {
+            var products = (await _cartService.GetDbCartProducts()).Data;
+            var lineItems = new List<SessionLineItemOptions>();
+            products.ForEach(product => lineItems.Add(new SessionLineItemOptions
+            {
+                PriceData = new SessionLineItemPriceDataOptions
+                {
+                    UnitAmountDecimal = product.Price * 100,
+                    Currency = "usd",
+                    ProductData = new SessionLineItemPriceDataProductDataOptions
+                    {
+                        Name = product.Title,
+                        Images = new List<string> { product.ImageUrl }
+                    }
+                },
+                Quantity = product.Quantity
+            }));
+
+            var options = new SessionCreateOptions
+            {
+                CustomerEmail = _authService.GetUserEmail(),
+                PaymentMethodTypes = new List<string>
+                {
+                    "card"
+                },
+                LineItems = lineItems,
+                Mode = "payment",
+                SuccessUrl = "https://localhost:7283/order-success",
+                CancelUrl = "https://localhost:7283/cart"
+            };
+
+            var service = new SessionService();
+            Session session = service.Create(options);
+            return session;
+        }
+    }
+}
